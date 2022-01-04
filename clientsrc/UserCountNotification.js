29822bc0cffa542c99a339b026602153e017740d
carl <carlco@gmail.com>
2022-01-04 10:57:29 +0100

    Rework of usercount notification to support a state-based flow with proper global and local state.

--- clientsrc/UserCountNotification.js
+++ clientsrc/UserCountNotification.js
@@ -1,36 +1,194 @@
 const RangeWindowQuery = require("./RangeWindowQuery");
 const DFU = require('./dfutil');
 
+// instantiated once per subscription.
+class UserCountNotificationIntegrationState {
+   constructor(integrationSpec, mgr, integrationID) {
+      this.mgr = mgr;
+      this.integrationSpec = integrationSpec;
+      this.integrationID = integrationID;
+
+      this.subscriptions = [];
+
+      this.uniqueMessages = new Map(); // map messagekey to time when first sent
+      this.joinTimer = null;
+      this.partTimer = null;
+
+      // set defaults & validate spec
+      this.integrationSpec.enabled ??= true;
+      this.integrationSpec.messageKey ??= '';
+      this.integrationSpec.messageKey = this.integrationSpec.messageKey.toString(); // avoid numeric types etc.
+      this.integrationSpec.uniqueMessageAge ??= '4h';
+      this.uniqueMessageAgeMS = RangeWindowQuery.DurationSpecToMS(mgr.ReplaceQueryVariables(this.integrationSpec.uniqueMessageAge));
+
+      this.joinDelayMS = RangeWindowQuery.DurationSpecToMS(mgr.ReplaceQueryVariables(integrationSpec.join.delay));
+      this.joinQuery = new RangeWindowQuery.RangeDurationQuery(mgr.ReplaceQueryVariables(integrationSpec.join.query));
+      console.assert(integrationSpec.join.messageContent);
+
+      this.partIntevalMS = RangeWindowQuery.DurationSpecToMS(mgr.ReplaceQueryVariables(integrationSpec.part.interval));
+      this.partQuery = new RangeWindowQuery.RangeDurationQuery(mgr.ReplaceQueryVariables(integrationSpec.part.query));
+      console.assert(integrationSpec.part.messageContent);
+
+      // warm up data source
+      this.GetDataSource();
+
+      // similar to jam tracker, the "join query" will put us in "joined state", then we move to the "part" stage. this variable tracks that state.
+      this.joinedState = false;
+   }
+
+   RegisterSubscription(subscription) {
+      this.subscriptions.push(subscription);
+   }
+
+   GetDataSource() {
+      const ds = this.mgr.GetDataSource(this.integrationSpec.dataSourceID);
+      return ds.globalDataSet;
+   }
+
+   PruneMessageKeys() {
+      const boundary = Date.now() - this.uniqueMessageAgeMS;
+      const keysToDelete = [];
+      this.uniqueMessages.forEach((v, k) => {
+         if (v <= boundary)
+            keysToDelete.push(k);
+      });
+      if (keysToDelete.length) {
+         if (this.integrationSpec.verboseDebugLogging) {
+            console.log(`${this.integrationID} Deleting ${keysToDelete.length} message keys:`);
+            keysToDelete.forEach(k => { console.log(`  - ${k}`); });
+         }
+      }
+      keysToDelete.forEach(k => this.uniqueMessages.delete(k));
+   }
+
+   Log(msg) {
+      this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} ${msg}`);
+   }
+
+   SendMessage(mustBeUnique, eventState, messageContent, messageFields) {
+      const subscription = this.subscriptions.find(s => s.roomID === eventState.roomID);
+      console.assert(subscription, `How can we be catch join/parts to a room, but the subscription hasn't been registered to global state?`);
+
+      let substitutions = {};
+      substitutions[`%roomName%`] = eventState.roomTitle;
+      substitutions[`%userName%`] = eventState.userName;
+      substitutions['%roomUserCount%'] = this.mgr._7jamAPI.Get7JamUserCountForRoom(subscription.roomID);
+      substitutions['%roomNoteCount%'] = this.mgr._7jamAPI.Get7JamNoteCountForRoom(subscription.roomID).toLocaleString();
+
+      let messageText = DFU.PerformSubstitutions(messageContent, substitutions);
+
+      if (mustBeUnique && this.integrationSpec.messageKey.length) {
+         let messageKey = DFU.PerformSubstitutions(this.integrationSpec.messageKey, substitutions);
+         this.PruneMessageKeys();
+         if (this.uniqueMessages.has(messageKey)) {
+            this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Suppressing notification because we already sent it.`);
+            return false;
+         }
+
+         this.uniqueMessages.set(messageKey, Date.now());
+      }
+
+      console.log(`${this.integrationID} ** sending discord notification: ${messageText}`);
+
+      this.mgr.bot.SendDiscordEmbedMessage(subscription.discordChannelID, eventState.absoluteURL,
+                                           messageText,
+                                           DFU.ProcessMessageFields(messageFields, substitutions));
+      return true;
+   }
+
+   On7jamUserPart(roomState, user) {
+      this.lastPartEventState = {
+         roomID:roomState.roomID,
+         roomTitle: roomState.roomTitle,
+         absoluteURL: roomState.absoluteURL,
+         userName: user.name,
+      };
+   }
+
+   On7jamUserJoin(roomState, user) {
+      if (this.joinedState)
+         return; // once joined state, we operate on an interval timer so no processing needed.
+
+      if (!this.integrationSpec.enabled) {
+         this.Log(`Suppressing HandleRoomUserCountChange because disabled.`);
+         return;
+      }
+
+      if (this.joinTimer) {
+         this.Log(`Replacing existing join timer`);
+         clearTimeout(this.joinTimer);
+      }
+      this.Log(`Setting join timer for ${this.joinDelayMS} ms`);
+
+      const eventState = {
+         roomID:roomState.roomID,
+         roomTitle: roomState.roomTitle,
+         absoluteURL: roomState.absoluteURL,
+         userName: user.name,
+      };
+
+      this.joinTimer = setTimeout(() => this.JoinDelayTimerProc(eventState), this.joinDelayMS);
+   }
+
+   JoinDelayTimerProc(eventState) {
+      this.joinTimer = null;
+      const joinQueryPass = this.GetDataSource().IsMatch(this.joinQuery, this.integrationID, this.integrationSpec.verboseDebugLogging);
+      if (!joinQueryPass) {
+         this.Log(`join query NOT met: ${this.joinQuery.spec}`);
+         return;
+      }
+      this.Log(`Join query PASSes.`);
+
+      // don't enter joined state if we don't send the notification. the whole point of doing this is to make sure notification
+      // messages are correllated so...
+      if (!this.SendMessage(true, eventState, this.integrationSpec.join.messageContent, this.integrationSpec.join.messageFields)) {
+         this.Log(`Message did not send; don't enter joined state.`);
+         return;
+      }
+
+      this.Log(`Entering joined state and setting part interval timer for ${this.partIntevalMS} ms`);
+      this.joinedState = true;
+      this.partTimer = setTimeout(() => this.PartIntervalTimerProc(), this.partIntevalMS);
+   }
+
+   PartIntervalTimerProc() {
+      this.partTimer = null;
+
+      const queryPass = this.GetDataSource().IsMatch(this.partQuery, this.integrationID, this.integrationSpec.verboseDebugLogging);
+      if (!queryPass) {
+         this.Log(`part query NOT met: ${this.partQuery.spec}, resetting part interval ${this.partIntevalMS} ms`);
+         this.partTimer = setTimeout(() => this.PartIntervalTimerProc(), this.partIntevalMS);
+         return;
+      }
+
+      this.Log(`Part query PASSes; exiting joined state.`);
+      this.joinedState = false;
+
+      this.SendMessage(false, this.lastPartEventState, this.integrationSpec.part.messageContent, this.integrationSpec.part.messageFields);
+   }
+};
+
+const gStateMap = new Map(); // map integrationID to state object
+
 class UserCountNotification {
    get RequiresUserListSync() {
       return false;
    }
+
    constructor(subscription, integrationSpec, mgr, integrationID) {
       this.mgr = mgr;
       this.subscription = subscription;
       this.integrationSpec = integrationSpec;
       this.integrationID = integrationID;
 
-      // set defaults
-      this.integrationSpec.enabled ??= true;
-      this.integrationSpec.silent ??= false;
-      this.integrationSpec.messageKey ??= '';
-      this.integrationSpec.uniqueMessageAge ??= '4h';
+      if (!gStateMap.has(integrationID)) {
+         gStateMap.set(integrationID, new UserCountNotificationIntegrationState(integrationSpec, mgr, integrationID));
+      }
+      gStateMap.get(integrationID).RegisterSubscription(subscription);
+   }
 
-      this.lastSentTimeMS = 0;
-      console.assert(!!integrationSpec.triggerOnUserCount, `UserCountNotification triggerOnUserCount is required`);
-      console.assert(!!integrationSpec.conditionOnUserCount, `UserCountNotification conditionOnUserCount is required`);
-      this.triggerQuery = new RangeWindowQuery.RangeDurationQuery(mgr.ReplaceQueryVariables(integrationSpec.triggerOnUserCount));
-      this.conditionQuery = new RangeWindowQuery.RangeDurationQuery(mgr.ReplaceQueryVariables(integrationSpec.conditionOnUserCount));
-      this.delayMS = 10 + RangeWindowQuery.DurationSpecToMS(mgr.ReplaceQueryVariables(integrationSpec.delay)); // add  for a margin when we recheck the query.
-      this.uniqueMessageAgeMS = RangeWindowQuery.DurationSpecToMS(mgr.ReplaceQueryVariables(this.integrationSpec.uniqueMessageAge));
-
-      this.uniqueMessages = new Map(); // map messagekey to time when first sent
-
-      this.timer = null;
-
-      // warm up data source
-      this.GetDataSource();
+   GetState() {
+      return gStateMap.get(this.integrationID);
    }
 
    GetAdminHelp() {
@@ -39,7 +197,6 @@ class UserCountNotification {
          "Commands:",
          "  log [0,1]      Enable verbose logging",
          "  enable [0,1]   Enables/disables this integration (no processing)",
-         "  silent [0,1]   Suppress the notification sent",
       ];
    }
 
@@ -47,7 +204,6 @@ class UserCountNotification {
       return [
          `Verbose logging: ${this.integrationSpec.verboseDebugLogging ? "on" : "off"}`,
          `Enabled        : ${this.integrationSpec.enabled ? "yes" : "no"}`,
-         `Silent         : ${this.integrationSpec.silent ? "yes" : "no"}`,
       ];
    }
 
@@ -67,126 +223,30 @@ class UserCountNotification {
          adminLogFn(`Now:  ${this.integrationSpec.enabled ? "enabled" : "disabled"}`);
          return;
       }
-      if (args[0] == 'silent') {
-         this.integrationSpec.silent = !!parseInt(args[1]);
-         adminLogFn(`${this.integrationSpec.silent ? "Notifications will now be suppressed" : "Notifications will be allowed to be sent."}`);
-         return;
-      }
       adminLogFn(`Unknown arg ${args[0]}`);
    }
 
    GetDebugData() {
+      const state = this.GetState();
       return {
          integrationID : this.integrationID,
-         preCondition : this.conditionQuery.spec,
-         delayMS : this.delayMS,
-         triggerQuery : this.triggerQuery.spec,
+         joinDelayMS : state.joinDelayMS,
+         joinQuery : state.joinQuery.spec,
+         partIntervalMS : state.partIntevalMS,
+         partQuery : state.partQuery.spec,
+         joinedState : state.joinedState,
       };
    }
 
-   GetDataSource() {
-      const ds = this.mgr.GetDataSource(this.integrationSpec.dataSourceID);
-      if (this.integrationSpec.userCountType === 'global') {
-         return ds.globalDataSet;
-      }
-      return ds.GetDataSourceForRoom(this.subscription.roomID);
-   }
-
-   PruneMessageKeys() {
-      //this.uniqueMessages = new Map(); // map messagekey to time when first sent
-      const boundary = Date.now() - this.uniqueMessageAgeMS;
-      const keysToDelete = [];
-      this.uniqueMessages.forEach((v, k) => {
-         if (v <= boundary) keysToDelete.push(k);
-      });
-      if (keysToDelete.length) {
-         if (this.integrationSpec.verboseDebugLogging) {
-            console.log(`${this.integrationID} Deleting ${keysToDelete.length} message keys:`);
-            keysToDelete.forEach(k => { console.log(`  - ${k}`); });
-         }
-      }
-      keysToDelete.forEach(k => this.uniqueMessages.delete(k));
-   }
-
-
-   // treat both JOIN and PART the same because either way we just want to examine the absolute user count.
    On7jamUserPart(roomState, user, roomUserCount, isJustChangingRoom) {
-      this.HandleRoomUserCountChange(roomState, user, roomUserCount, isJustChangingRoom);
+      this.GetState().On7jamUserPart(roomState, user);
    }
 
    On7jamUserJoin(roomState, user, roomUserCount, isJustChangingRoom) {
-      this.HandleRoomUserCountChange(roomState, user, roomUserCount, isJustChangingRoom);
+      this.GetState().On7jamUserJoin(roomState, user);
    }
 
-   HandleRoomUserCountChange(roomState, user, roomUserCount, isJustChangingRoom) {
-      if (!this.integrationSpec.enabled) {
-         this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Suppressing HandleRoomUserCountChange because disabled.`);
-         return;
-      }
-
-      let conditionMet = this.GetDataSource().IsMatch(this.conditionQuery, this.integrationID, this.integrationSpec.verboseDebugLogging);
-
-      if (!conditionMet) {
-         this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} PreCondition NOT met: ${this.conditionQuery.spec}`);
-         return;
-      }
-
-      this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} PreCondition MET: ${this.conditionQuery.spec}`);
-
-      const timerProc = () => {
-         if (!this.integrationSpec.enabled) {
-            this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Suppressing timerProc because disabled.`);
-            return;
-         }
-
-         this.timer = null;
-
-         // check condition.
-         if (!this.GetDataSource().IsMatch(this.triggerQuery, this.integrationID, this.integrationSpec.verboseDebugLogging)) {
-            this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Trigger NOT met: ${this.triggerQuery.spec}`);
-            return;
-         }
-
-         this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Trigger MET: ${this.triggerQuery.spec}`);
-
-         if (this.integrationSpec.silent) {
-            this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Suppressing notification because of silent mode.`);
-            return;
-         }
-
-         let substitutions = {};
-         substitutions[`%roomName%`] = roomState.roomTitle;
-         substitutions[`%userName%`] = user.name;
-         substitutions['%roomUserCount%'] = this.mgr._7jamAPI.Get7JamUserCountForRoom(this.subscription.roomID);
-         substitutions['%roomNoteCount%'] = this.mgr._7jamAPI.Get7JamNoteCountForRoom(this.subscription.roomID).toLocaleString();
-
-         let messageText = DFU.PerformSubstitutions(this.integrationSpec.messageContent, substitutions);
-         let messageKey = DFU.PerformSubstitutions(this.integrationSpec.messageKey, substitutions);
-
-         this.PruneMessageKeys();
-         if (this.uniqueMessages.has(messageKey)) {
-            this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} Suppressing notification because we already sent it.`);
-            return;
-         }
-
-         this.uniqueMessages.set(messageKey, Date.now());
-
-         console.log(`${this.integrationID} ** sending discord notification: ${messageText}`);
-         //this.subscription.RegisterNotificationSent(this.integrationSpec.groupName);
-
-         this.mgr.bot.SendDiscordEmbedMessage(this.subscription.discordChannelID, roomState.absoluteURL,
-                                              messageText,
-                                              DFU.ProcessMessageFields(this.integrationSpec.messageFields, substitutions));
-      };
-
-      if (this.timer) {
-         this.integrationSpec.verboseDebugLogging && console.log(`${this.integrationID} clearing existing timer`);
-         clearTimeout(this.timer);
-      }
-
-      this.timer = setTimeout(timerProc, this.delayMS);
-   }
-};
+}
 
 module.exports = {
    UserCountNotification,
