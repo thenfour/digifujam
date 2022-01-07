0c8b6bb40630208fceaa3d408a52277f9feca7b1
carl <carlco@gmail.com>
2022-01-07 14:43:30 +0100

    some npm upgrades

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,29 @@ module.exports = {
         '7jam': './clientsrc/7jam.js',
         '7jamAdmin': './clientsrc/7jamAdminClient.js'
     },
-    module: {
+    output: {
+        environment: {
+          // The environment supports arrow functions ('() => { ... }').
+          arrowFunction: true,
+          // The environment supports BigInt as literal (123n).
+          bigIntLiteral: false,
+          // The environment supports const and let for variable declarations.
+          const: true,
+          // The environment supports destructuring ('{ a, b } = obj').
+          destructuring: true,
+          // The environment supports an async import() function to import EcmaScript modules.
+          dynamicImport: false,
+          // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+          forOf: true,
+          // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+          module: false,
+          // The environment supports optional chaining ('obj?.a' or 'obj?.()').
+          optionalChaining: true,
+          // The environment supports template literals.
+          templateLiteral: true,
+        },
+      },
+          module: {
         rules: [
             {
                 test: /\.js$/,
