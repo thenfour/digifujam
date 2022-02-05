1cda70a9e9632bf7d234bd345c8074a311d4007f
carl <carlco@gmail.com>
2022-02-05 21:28:05 +0100

    adding "Ideas?" dialog sequencer leaves a bit of room to see chat messages

--- clientsrc/ui/right.js
+++ clientsrc/ui/right.js
@@ -10,7 +10,7 @@ const UIUser = require("./UIUser");
 const DFU = require('../dfutil');
 const DFOptionsDialog = require('./optionsDialog');
 const KeybDisplayState = require("./keybDisplayState");
-const CreditsButton = require("./CreditsButton");
+const {CreditsButton, IdeasButton, IdeasDialog} = require("./CreditsButton");
 const {SequencerMain} = require("./SequencerMain");
 const {InlinePitchBendCtrl, InlineMasterGainCtrl} = require('./InlinePitchBendCtrl');
 const {UserSettingsButton} = require("./userSettings");
@@ -19,6 +19,7 @@ const {GestureSplash} = require('./splash');
 const {SequencerParamGroup} = require('./SequencerParamGroup');
 const {DFAlert} = require('./DFAlert');
 const {GraffitiScreen, GraffitiContainer} = require("./graffiti");
+const EventEmitter = require('events');
 
 const md = require('markdown-it')({
     html:         false,        // Enable HTML tags in source
@@ -27,6 +28,10 @@ const md = require('markdown-it')({
     linkify:      true,        // Autoconvert URL-like text to links
     typographer:  false,
   });
+
+// just simpler than passing around interfaces everywhere, and allows subscription
+// where callers (App e.g.) don't care.
+window.DFEvents = new EventEmitter();
  
 // https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
 // this adds attribute target=_blank so links open in new tab.
@@ -2755,6 +2760,7 @@ class RootArea extends React.Component {
             <div id="allContentContainer" onKeyDown={this.onKeyDown}>
             <GestureSplash app={this.state.app}></GestureSplash>
             <DFAlert></DFAlert>
+            <IdeasDialog></IdeasDialog>
             <div id="grid-container" style={gridContainerStyle}>
                 <div style={{ gridArea: "headerArea", textAlign: 'center' }} className="headerArea">
                     <span>
@@ -2782,6 +2788,7 @@ class RootArea extends React.Component {
                             <svg className="socicon" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
                         </a>
                         <CreditsButton></CreditsButton>
+                        <IdeasButton />
                     </span>
                 </div>
                 <DFPiano.PianoArea app={this.state.app} />
