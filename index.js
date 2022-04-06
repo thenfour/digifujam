8db6a04ea31860db5a08ef562181f67c5663b8c1
carl <carlco@gmail.com>
2022-04-06 12:41:48 +0200

    increase buffer size to support admin import state

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  maxHttpBufferSize: 5000000, // make big enough to support import/export of whole server state.
+});
 
 const { _7jamServer } = require('./source/DFserver/7jamserver');
 
